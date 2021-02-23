67d5ed3612bf351692bc77e76d845dca4a86ee2c
verno3632 <verno3632@pixiv.com>
2021-02-24 08:57:23 +0900

    global runをやめる

--- script/test.sh
+++ script/test.sh
@@ -2,13 +2,11 @@
 
 mkdir -p ./test-reports/ 2>/dev/null
 
-flutter pub global activate junitreport 
-flutter pub global activate dart_dot_reporter
 curl -kL https://bootstrap.pypa.io/get-pip.py | python
 pip install junit2html
 
 flutter test --machine > machine.json
-flutter pub global run junitreport:tojunit --input machine.json --output ./test-reports/TEST-results.xml
+flutter pub run junitreport:tojunit --input machine.json --output ./test-reports/TEST-results.xml
 echo 'Tests done.'
-flutter pub global run dart_dot_reporter machine.log
+flutter pub run dart_dot_reporter machine.json
 junit2html ./test-reports/TEST-results.xml ./test-reports/index.html
